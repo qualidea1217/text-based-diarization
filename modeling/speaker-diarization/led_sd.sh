211d8f585606dffdff6600022e399c9217d3ec80
qualidea1217 <peilinwu1217@gmail.com>
2023-08-30 15:38:35 -0400

    Update: 8/30 update2, change naming

--- modeling/speaker-diarization/led_sd.sh
+++ modeling/speaker-diarization/led_sd.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=longformer_sd
-#SBATCH --output=longformer_sd_out.txt
+#SBATCH --job-name=led_sd
+#SBATCH --output=led_sd_out.txt
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 
 cd /local/scratch/pwu54/python/text-based-diarization/modeling/speaker-diarization/
-python3 longformermodel.py
+python3 ledmodel.py
